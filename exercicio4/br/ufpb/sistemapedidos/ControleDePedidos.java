a47b732a89b43a1b88482d00a39edd2973d0faf5
ahemenson <ahemenson.cavalcante@dce.ufpb.br>
2012-11-13 19:45:23 -0300

    adicionando exercicio 4

---
+++ exercicio4/br/ufpb/sistemapedidos/ControleDePedidos.java
@@ -0,0 +1,73 @@
+package br.ufpb.sistemapedidos;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Controla os pedidos do cliente
+ *
+ */
+public class ControleDePedidos implements InterfaceSistemaDeControle {
+
+	private List <Pedido> pedidos;
+	
+	public ControleDePedidos(){
+		this.pedidos = new ArrayList<Pedido>();
+	}
+	/**
+	 * busca a quantidade de pedidos efetuados por determinado cliente
+	 * @return retorna a quantidade de pedidos
+	 */
+	public int calculaQuantidadeDePedidosDoCliente(String nomeCliente){
+		
+		int cont = 0;
+		for (Pedido p: this.pedidos){
+			if (p.getCliente().getNome().equals(nomeCliente)){
+				cont++;
+			}
+		}
+		return cont;
+	}
+	/**
+	 * adiciona um pedido do cliente na lista
+	 * @param objeto pedido	
+	 */
+	public void adicionaPedido(Pedido p) {
+		this.pedidos.add(p);
+
+	}
+
+	/**
+	 * pesquisa na lista de pedidos através do código de um produto 
+	 * @param é passado o codigo do produto
+	 * @return retorna uma lista com os produtos encontrados com o codigo correspondente
+	 */
+	public List<Pedido> pesquisaPedidosIncluindoProduto(String codProduto) {
+		List <Pedido> pedidosProcurados = new LinkedList<Pedido>();
+		for (Pedido p: this.pedidos){
+			List<ItemDePedido> itens = p.getItens();
+			for (ItemDePedido it: itens){
+				if (it.getCodProduto().equals(codProduto)){
+					pedidosProcurados.add(p);
+				}
+			}
+		}
+		//TODO: ajeitar para verificar itens repetidos
+		return pedidosProcurados;
+	}
+
+	/**
+	 * remove um pedido da lista por meio do número do pedido
+	 * @param é passado o numero do Pedido
+	 */
+	public void removePedido(long numeroPedido) {
+		for (Pedido p: this.pedidos){
+			if (p.getNumeroPedido()== numeroPedido){
+				this.pedidos.remove(p);
+				break;
+			}
+		}
+
+	}
+
+}
---
+++ exercicio4/br/ufpb/sistemapedidos/Principal.java
@@ -0,0 +1,50 @@
+package br.ufpb.sistemapedidos;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		double valor = 0;		
+		
+		System.out.println("informe seus três pedidos");
+				
+		int numPedido=1;
+				
+		boolean continua = true;
+		do{
+			System.out.println("cod produto");
+			String cod=input.next();
+			
+			System.out.println("quant produto");
+			String quant=input.next();
+			
+			System.out.println("valor unitário do produto");
+			String valorUnitario=input.next();
+									
+			if(cod.equals("0")||quant.equals("0")||valorUnitario.equals("0")){
+						
+				continua=false;
+				quant="0";
+				valorUnitario="0";
+			}
+							
+			ItemDePedido itens = new ItemDePedido(cod,Integer.parseInt(quant),Double.parseDouble(valorUnitario));
+								
+			Pedido pedido = new Pedido(numPedido);
+			pedido.adicionaItem(itens);
+			valor=pedido.getValorTotal()+valor;
+						
+			numPedido++;
+		
+		}while (continua!=false);
+		
+		System.out.println(" Valor total:R$ ["+valor+"]");
+		
+	}
+
+}
